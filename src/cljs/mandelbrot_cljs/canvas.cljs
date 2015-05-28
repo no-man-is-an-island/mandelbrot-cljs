dfe301b304345b5e2200524baeb6b5358356928b
David Williams <dpw341@gmail.com>
2015-05-28 10:41:28 +0100

    Break out namespace of canvas helper functions

---
+++ src/cljs/mandelbrot_cljs/canvas.cljs
@@ -0,0 +1,38 @@
+(ns mandelbrot-cljs.canvas
+  "Some helper functions for using HTML5 Canvas")
+
+(defn set-canvas-dimensions!
+  "Sets the canas to the same size as the enclosing window"
+  [canvas]
+  (aset canvas "width" (.-innerWidth js/window))
+  (aset canvas "height" (.-innerHeight js/window)))
+
+(defn clear-canvas!
+  "Clears a whole canvas"
+  [canvas]
+  (let [context (.getContext canvas "2d")]
+    (.clearRect context 0 0 (aget canvas "width") (aget canvas "height"))))
+
+(defn add-rectangle!
+  "Adds a (fill or stroke) rectangle to the canvas (and optionally clears all other rectangles)"
+  [canvas
+   x0 y0 x1 y1 & {:keys [clear? color opacity type] :or {clear? true color "green" opacity 1 type :stroke}}]
+  (let [context (.getContext canvas "2d")]
+    (aset context "globalAlpha" opacity)
+    (aset context "lineWidth" 1)
+
+
+    (when clear? (clear-canvas! canvas))
+
+    (case type
+
+      :stroke (do (aset context "strokeStyle" color)
+                  (.strokeRect context x0 y0 (- x1 x0) (- y1 y0)))
+
+      :fill (do (aset context "fillStyle" color)
+                (.fillRect context x0 y0 (- x1 x0) (- y1 y0))))))
+
+(defn open-as-png!
+  "Open a new tab or window containing a png version of the canvas"
+  [canvas]
+  (.open js/window (.toDataURL canvas "image/png")))
--- src/cljs/mandelbrot_cljs/core.cljs
+++ src/cljs/mandelbrot_cljs/core.cljs
@@ -1,4 +1,6 @@
 (ns mandelbrot-cljs.core
+  (:require [mandelbrot-cljs.canvas :refer [set-canvas-dimensions! clear-canvas!
+                                            add-rectangle! open-as-png!]])
   (:use-macros [mandelbrot-cljs.macros :only [forloop]]))
 
 (enable-console-print!)
@@ -25,37 +27,6 @@
    to something which should look ok on most screens"
   []
   (swap! app-state assoc :rendering-data initial-rendering-data))
-
-(defn set-canvas-dimensions!
-  "Sets the canas to the same size as the enclosing window"
-  [canvas]
-  (aset canvas "width" (.-innerWidth js/window))
-  (aset canvas "height" (.-innerHeight js/window)))
-
-(defn clear-canvas!
-  "Clears a whole canvas"
-  [canvas]
-  (let [context (.getContext canvas "2d")]
-    (.clearRect context 0 0 (aget canvas "width") (aget canvas "height"))))
-
-(defn add-rectangle!
-  "Adds a (fill or stroke) rectangle to the canvas (and optionally clears all other rectangles)"
-  [canvas
-   x0 y0 x1 y1 & {:keys [clear? color opacity type] :or {clear? true color "green" opacity 1 type :stroke}}]
-  (let [context (.getContext canvas "2d")]
-    (aset context "globalAlpha" opacity)
-    (aset context "lineWidth" 1)
-
-
-    (when clear? (clear-canvas! canvas))
-
-    (case type
-
-      :stroke (do (aset context "strokeStyle" color)
-                  (.strokeRect context x0 y0 (- x1 x0) (- y1 y0)))
-
-      :fill (do (aset context "fillStyle" color)
-                (.fillRect context x0 y0 (- x1 x0) (- y1 y0))))))
 
 (defn enclosing-rectangle
   "Calculates an enclosing rectangle which contains the given
@@ -141,7 +112,7 @@
                                                  (- (.getTime (js/Date.)) start)))) "px/s")))
 
 (defn re-render!
-  "Adds a green semi-transparent overlay to the screen, then
+  "Adds a blue semi-transparent overlay to the screen, then
   lines up a re-rendering (we need to yield control back to
   the browser so that the overlay actually get rendered)"
   [{:as new-state :keys [overlay-canvas]}]
@@ -225,11 +196,6 @@
   (set! (.-onmousemove overlay-canvas) handle-mousemove)
 
   (set! (.-onmouseup overlay-canvas) handle-mouseup))
-
-(defn open-as-png!
-  "Open a new tab or window containing a png version of the canvas"
-  [canvas]
-  (.open js/window (.toDataURL canvas "image/png")))
 
 (defn init!
   "Initialise event handlers, add atom watches, do the first rendering"
